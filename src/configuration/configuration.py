7d3e0f3dcdb052f2c438ebcf4d49dda87ecdbf2b
QimingFangS <52064608+QimingFangS@users.noreply.github.com>
2021-07-01 10:28:09 -0400

    Update configuration.py

--- src/configuration/configuration.py
+++ src/configuration/configuration.py
@@ -844,11 +844,11 @@ class configuration(object):
             
             try:
                 # pass that string as a filehandle
-                #fh = io.StringIO()
+                fh = io.StringIO(config_string)
                 #print('-----DEBUG-----', config_string)
                 #fh = logging.FileHandler('/home/csuser/deepvoice3_pytorch/vctk_preprocess/latest_features/merlin/egs/build_your_own_voice/s1/conf/logging_config.conf')
                 #fh = io.StringIO(config_string)
-                logging.config.fileConfig(fname = '/home/csuser/deepvoice3_pytorch/vctk_preprocess/latest_features/merlin/egs/build_your_own_voice/s1/conf/logging_config.conf')
+                logging.config.fileConfig(fh)
                 fh.close()
                 logger.info("logging is now fully configured")
 
